6be1a39c9d68282233e49385a9fb9daf7a1df7cb
morix1500 <rade1500@gmail.com>
2018-09-15 22:38:24 +0900

    first commit

---
+++ content_script.js
@@ -0,0 +1,51 @@
+﻿//----------------------
+$(function () {
+    let host = location.host;
+
+    if (host != "www.google.co.jp") {
+      highlight();
+      return;
+    }
+    let params = location.search.substring(1).split('&');
+    let q = "";
+    for (let i = 0; i < params.length; i++) {
+      let kv = params[i].split("=");
+      if (kv[0] == "q") {
+        q = kv[1];
+        break;
+      }
+    }
+    if (q.length == 0) { return }
+
+    let obj = {};
+    obj.q = q;
+    chrome.storage.local.set(obj, null);
+});
+
+chrome.runtime.onMessage.addListener(
+  function(req, sender, res) {
+    if (req.type == "update") {
+      highlight();
+    } else {
+      $("body").removeHighlight();
+    }
+  }
+);
+
+function highlight() {
+  chrome.storage.local.get(function(obj) {
+    if (("q" in obj) == false) { return }
+    //if (Object.keys(obj.q).length == 0) { return }
+    $("body").removeHighlight();
+
+    let color = "#ffff00";
+    if (Object.keys(obj.color).length > 0) { color = obj.color }
+
+    let words = obj.q.split('+');
+    for (let i = 0; i < words.length; i++) {
+      let word = decodeURIComponent(words[i]);
+      $("body").highlight(word);
+    }
+    $(".highlight-ext").css("background-color", color);
+  });
+}
---
+++ popup_script.js
@@ -0,0 +1,55 @@
+﻿//----------------------
+$(function () {
+  chrome.storage.local.get(function(obj) {
+    if (("q" in obj) == false) { return }
+    let words = obj.q.split('+');
+    let text = "";
+
+    for (let i = 0; i < words.length; i++) {
+      let word = decodeURIComponent(words[i]);
+      word = escapeHtml(word);
+      text += word + " ";
+    }
+    $("#word").html(text);
+    if ("color" in obj) {
+      $("input[name='color']").val(obj.color);
+    }
+  });
+  $("#reset").on("click", function() {
+    reset();
+  });
+  $("#save").on("click", function() {
+    updateHighLightColor();
+  });
+});
+
+function reset() {
+  chrome.storage.local.remove("q", null);
+  $("#word").html("");
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {type: "reset"}, function(res) {
+    });
+  });
+}
+
+function updateHighLightColor() {
+  let obj = {};
+  obj.color = $("input[name='color']").val();
+
+  chrome.storage.local.set(obj, function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: "update"}, null);
+    });
+  });
+}
+
+function escapeHtml(str){
+  str = str.replace(/&/g, '&amp;');
+  str = str.replace(/>/g, '&gt;');
+  str = str.replace(/</g, '&lt;');
+  str = str.replace(/"/g, '&quot;');
+  str = str.replace(/'/g, '&#x27;');
+  str = str.replace(/`/g, '&#x60;');
+  return str;
+}
